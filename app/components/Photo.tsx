80121c98bc707708dfd2d45d01553a393c5ba6e6
DINESH <muktinath.bastakoti2080@macpokhara.edu.np>
2024-12-16 17:42:45 +0545

    all set

--- app/components/Photo.tsx
+++ app/components/Photo.tsx
@@ -18,7 +18,8 @@ const Photo = () => {
             opacity: 1,
             transition: { delay: 2.4, duration: 0.4, ease: "easeInOut" },
           }}
-          className="w-[298px] h-[298px] xl:w-[498px] xl:h-498px] mix-blend-lighten absolute">
+          className="w-[298px] h-[298px] xl:w-[498px] xl:h-498px] mix-blend-lighten absolute"
+          style={{ right: "2px", left: "4px", top: "-5px", bottom: "0" }}>
           <Image
             src="/assets/photo.png"
             priority
@@ -29,14 +30,15 @@ const Photo = () => {
             className="object-contain"
           />
         </motion.div>
+
         <motion.svg
           className="w-[300px] xl:w-[506px] h-[300px] xl:h=[506px]"
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="https://www.w3.org/2000/svg">
           <motion.circle
-            cx="253"
-            cy="253"
+            cx="252"
+            cy="252"
             r="250"
             stroke="#00ff99"
             strokeWidth="4"
