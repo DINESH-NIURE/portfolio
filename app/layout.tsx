acf912e249bde8e04da59a41916123dff832c808
DINESH <muktinath.bastakoti2080@macpokhara.edu.np>
2024-12-17 13:41:42 +0545

    vercel insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -26,6 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <SpeedInsights />
       <body className={jetbrainsMono.variable}>
         <Header />
         <PageTransition>{children}</PageTransition>
